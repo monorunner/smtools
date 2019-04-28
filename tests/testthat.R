19fde68ab950e2410fcb50cdc50ee1fa7a387634
monorunner <monorunner@users.noreply.github.com>
2019-04-28 18:39:28 +0100

    Add initial tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(smtools)
+
+test_check("smtools")
---
+++ tests/testthat/test-smtools.R
@@ -0,0 +1,7 @@
+context('Test smtools')
+library(smtools)
+
+test_that("clean.names works", {
+  expect_equal(clean.names(data.table(AsjkB=1)), data.table(asjkb=1))
+  expect_equal(clean.names(data.table(sa_ds_1=1)), data.table(sa.ds1=1))
+})
