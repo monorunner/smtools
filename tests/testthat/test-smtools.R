7b65f1f13e66124f48ca98ee92103d532f72bf07
monorunner <monorunner@users.noreply.github.com>
2019-04-28 18:48:07 +0100

    Add excel.names test

--- tests/testthat/test-smtools.R
+++ tests/testthat/test-smtools.R
@@ -1,7 +1,11 @@
-context('Test smtools')
+context("Test smtools")
 library(smtools)
 
 test_that("clean.names works", {
   expect_equal(clean.names(data.table(AsjkB=1)), data.table(asjkb=1))
   expect_equal(clean.names(data.table(sa_ds_1=1)), data.table(sa.ds1=1))
+})
+
+test_that("excel.names works",{
+  expect_equal(excel.names(data.table(po.zf=1)), data.table(`Po Zf`=1))
 })
